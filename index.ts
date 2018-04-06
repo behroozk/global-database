4f591d9d2bbd2223d0996d71956479be64dce086
Behrooz Kamali <behroozkamali@yahoo.com>
2018-04-05 23:42:26 -0400

    add main package index file

---
+++ index.ts
@@ -0,0 +1,11 @@
+import { Mysql } from './src/mysql';
+import { IMysqlOptions } from './src/mysql/options.interface';
+import { Neo4j } from './src/neo4j';
+import { INeo4jOptions } from './src/neo4j/options.interface';
+
+export {
+    IMysqlOptions,
+    Mysql,
+    INeo4jOptions,
+    Neo4j,
+};
