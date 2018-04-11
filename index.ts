033b46267e924e3e1f23203c5efd5a6efbaa64ec
Behrooz Kamali <behroozkamali@yahoo.com>
2018-04-10 21:26:14 -0400

    add client interface to the exported objects

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
+import { IClient } from './src/client/interface';
 import { Mysql } from './src/mysql';
 import { IMysqlOptions } from './src/mysql/options.interface';
 import { Neo4j } from './src/neo4j';
 import { INeo4jOptions } from './src/neo4j/options.interface';
 
 export {
+    IClient,
     IMysqlOptions,
     Mysql,
     INeo4jOptions,
