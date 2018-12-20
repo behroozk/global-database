231b4d689f34647d47597c0d4c8c4337ee8b0d11
Behrooz Kamali <behroozkamali@yahoo.com>
2018-12-20 09:30:45 -0500

    export query options interface

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { RecordAlreadyExistsError } from './src/client/error/record_already_exists.error';
 import { IClient } from './src/client/interface';
 import { LogLevel } from './src/client/options.interface';
+import { IQueryOptions } from './src/client/query_options.interface';
 import { Mysql } from './src/mysql';
 import { IMysqlOptions } from './src/mysql/options.interface';
 import { Neo4j } from './src/neo4j';
@@ -8,6 +9,7 @@ import { INeo4jOptions } from './src/neo4j/options.interface';
 
 export {
     IClient,
+    IQueryOptions,
     IMysqlOptions,
     INeo4jOptions,
     LogLevel,
